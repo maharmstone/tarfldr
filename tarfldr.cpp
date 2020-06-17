3d27c7c4f62a11d8b549a2b74bfd85a3e51fdf8a
Mark Harmstone <mark@harmstone.com>
2020-06-17 03:00:46 +0100

    DllGetClassObject: return factory object

--- tarfldr.cpp
+++ tarfldr.cpp
@@ -1,12 +1,77 @@
 #include <windows.h>
 
+static const GUID CLSID_TarFldr = { 0x95b57a60, 0xcb8e, 0x49fc, { 0x8d, 0x4c, 0xef, 0x12, 0x25, 0x20, 0x0d, 0x7d } };
+
+LONG objs_loaded = 0;
+
+// FIXME - installer
+
 extern "C" STDAPI DllCanUnloadNow(void) {
-    // FIXME
-    return S_OK;
+    return objs_loaded == 0 ? S_OK : S_FALSE;
 }
 
+class Factory : public IClassFactory {
+public:
+    Factory() {
+        InterlockedIncrement(&objs_loaded);
+    }
+
+    virtual ~Factory() {
+        InterlockedDecrement(&objs_loaded);
+    }
+
+    // IUnknown
+
+    HRESULT __stdcall QueryInterface(REFIID iid, void** ppv) {
+        if (iid == IID_IUnknown || iid == IID_IClassFactory)
+            *ppv = static_cast<IClassFactory*>(this);
+        else {
+            *ppv = nullptr;
+            return E_NOINTERFACE;
+        }
+
+        reinterpret_cast<IUnknown*>(*ppv)->AddRef();
+
+        return S_OK;
+    }
+
+    ULONG __stdcall AddRef() {
+        return InterlockedIncrement(&refcount);
+    }
+
+    ULONG __stdcall Release() {
+        LONG rc = InterlockedDecrement(&refcount);
+
+        if (rc == 0)
+            delete this;
+
+        return rc;
+    }
+
+    // IClassFactory
+
+    HRESULT __stdcall CreateInstance(IUnknown* pUnknownOuter, const IID& iid, void** ppv) {
+        __asm("int $3");
+        // FIXME - IID_IShellFolder
+        return E_FAIL;
+    }
+
+    HRESULT __stdcall LockServer(BOOL bLock) {
+        return E_NOTIMPL;
+    }
+
+private:
+    LONG refcount = 0;
+};
+
 extern "C" STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID* ppv) {
-    // FIXME
+    if (rclsid == CLSID_TarFldr) {
+        Factory* fact = new Factory;
+        if (!fact)
+            return E_OUTOFMEMORY;
+        else
+            return fact->QueryInterface(riid, ppv);
+    }
 
     return CLASS_E_CLASSNOTAVAILABLE;
 }
