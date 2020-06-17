4bf86b4db4d5a86eda723d0399b2dbc366a4b96e
Mark Harmstone <mark@harmstone.com>
2020-06-17 22:47:08 +0100

    add stub for IShellFolder

--- tarfldr.cpp
+++ tarfldr.cpp
@@ -1,4 +1,5 @@
 #include <windows.h>
+#include <shobjidl.h>
 
 static const GUID CLSID_TarFldr = { 0x95b57a60, 0xcb8e, 0x49fc, { 0x8d, 0x4c, 0xef, 0x12, 0x25, 0x20, 0x0d, 0x7d } };
 
@@ -10,13 +11,102 @@ extern "C" STDAPI DllCanUnloadNow(void) {
     return objs_loaded == 0 ? S_OK : S_FALSE;
 }
 
-class Factory : public IClassFactory {
+class shell_folder : public IShellFolder {
 public:
-    Factory() {
+    // IUnknown
+
+    HRESULT __stdcall QueryInterface(REFIID iid, void** ppv) {
+        if (iid == IID_IUnknown || iid == IID_IClassFactory)
+            *ppv = static_cast<IShellFolder*>(this);
+        else {
+            *ppv = nullptr;
+            return E_NOINTERFACE;
+        }
+
+        reinterpret_cast<IUnknown*>(*ppv)->AddRef();
+
+        return S_OK;
+    }
+
+    ULONG __stdcall AddRef() {
+        return InterlockedIncrement(&refcount);
+    }
+
+    ULONG __stdcall Release() {
+        LONG rc = InterlockedDecrement(&refcount);
+
+        if (rc == 0)
+            delete this;
+
+        return rc;
+    }
+
+    // IShellFolder
+
+    HRESULT __stdcall ParseDisplayName(HWND hwnd, IBindCtx *pbc, LPWSTR pszDisplayName, ULONG *pchEaten,
+                                       PIDLIST_RELATIVE *ppidl, ULONG *pdwAttributes) {
+        // FIXME
+        return E_NOTIMPL;
+    }
+
+    HRESULT __stdcall EnumObjects(HWND hwnd, SHCONTF grfFlags, IEnumIDList **ppenumIDList) {
+        // FIXME
+        return E_NOTIMPL;
+    }
+
+    HRESULT __stdcall BindToObject(PCUIDLIST_RELATIVE pidl, IBindCtx *pbc, REFIID riid, void **ppv) {
+        // FIXME
+        return E_NOTIMPL;
+    }
+
+    HRESULT __stdcall BindToStorage(PCUIDLIST_RELATIVE pidl, IBindCtx *pbc, REFIID riid, void **ppv) {
+        // FIXME
+        return E_NOTIMPL;
+    }
+
+    HRESULT __stdcall CompareIDs(LPARAM lParam, PCUIDLIST_RELATIVE pidl1, PCUIDLIST_RELATIVE pidl2) {
+        // FIXME
+        return E_NOTIMPL;
+    }
+
+    HRESULT __stdcall CreateViewObject(HWND hwndOwner, REFIID riid, void **ppv) {
+        // FIXME
+        return E_NOTIMPL;
+    }
+
+    HRESULT __stdcall GetAttributesOf(UINT cidl, PCUITEMID_CHILD_ARRAY apidl, SFGAOF *rgfInOut) {
+        // FIXME
+        return E_NOTIMPL;
+    }
+
+    HRESULT __stdcall GetUIObjectOf(HWND hwndOwner, UINT cidl, PCUITEMID_CHILD_ARRAY apidl, REFIID riid,
+                                    UINT *rgfReserved, void **ppv) {
+        // FIXME
+        return E_NOTIMPL;
+    }
+
+    HRESULT __stdcall GetDisplayNameOf(PCUITEMID_CHILD pidl, SHGDNF uFlags, STRRET *pName) {
+        // FIXME
+        return E_NOTIMPL;
+    }
+
+    HRESULT __stdcall SetNameOf(HWND hwnd, PCUITEMID_CHILD pidl, LPCWSTR pszName, SHGDNF uFlags,
+                                PITEMID_CHILD *ppidlOut) {
+        // FIXME
+        return E_NOTIMPL;
+    }
+
+private:
+    LONG refcount = 0;
+};
+
+class factory : public IClassFactory {
+public:
+    factory() {
         InterlockedIncrement(&objs_loaded);
     }
 
-    virtual ~Factory() {
+    virtual ~factory() {
         InterlockedDecrement(&objs_loaded);
     }
 
@@ -51,9 +141,16 @@ public:
     // IClassFactory
 
     HRESULT __stdcall CreateInstance(IUnknown* pUnknownOuter, const IID& iid, void** ppv) {
-        __asm("int $3");
-        // FIXME - IID_IShellFolder
-        return E_FAIL;
+        if (iid == IID_IUnknown || iid == IID_IShellFolder) {
+            shell_folder* sf = new shell_folder;
+            if (!sf)
+                return E_OUTOFMEMORY;
+
+            return sf->QueryInterface(iid, ppv);
+        }
+
+        *ppv = nullptr;
+        return E_NOINTERFACE;
     }
 
     HRESULT __stdcall LockServer(BOOL bLock) {
@@ -66,7 +163,7 @@ private:
 
 extern "C" STDAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID* ppv) {
     if (rclsid == CLSID_TarFldr) {
-        Factory* fact = new Factory;
+        factory* fact = new factory;
         if (!fact)
             return E_OUTOFMEMORY;
         else
