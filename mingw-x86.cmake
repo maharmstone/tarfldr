f80418734263378b37b5421314d8865fd1b206a4
Mark Harmstone <mark@harmstone.com>
2020-07-05 15:33:01 +0100

    get cmake to strip DLL after link

--- mingw-x86.cmake
+++ mingw-x86.cmake
@@ -1,21 +1,11 @@
-#
-# To Cross-compile, use:
-#  cmake -DCMAKE_TOOLCHAIN_FILE=../mingw.cmake ..
-#
-# the name of the target operating system
 SET(CMAKE_SYSTEM_NAME Windows)
 
-# which compilers to use for C and C++
 SET(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
 SET(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
 SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
 
-# here is the target environment located
 SET(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32)
 
-# adjust the default behaviour of the FIND_XXX() commands:
-# search headers and libraries in the target environment, search
-# programs in the host environment
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
@@ -25,3 +15,6 @@ set(CMAKE_PREFIX_PATH /usr/i686-w64-mingw32/usr/lib/cmake)
 set(CMAKE_SYSTEM_PROCESSOR x86)
 
 add_link_options(-Wl,--enable-stdcall-fixup)
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -s")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -s")
